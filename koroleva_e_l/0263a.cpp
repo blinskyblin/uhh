482fbffd05e99b6854dab9a25499cd59a5147fd0
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-10-10 21:24:48 +0300

    Update 0263a.cpp

--- koroleva_e_l/0263a.cpp
+++ koroleva_e_l/0263a.cpp
@@ -3,11 +3,10 @@
  
 int main()
 {
-  
-	int x;
-	for (int i = 1; i <= 5; ++i) {
-		for (int j = 1; j <= 5; ++j) {
-			std::cin >> x;
-	        if (x == 1) {
-				std::cout << abs(i - 3) + abs(j - 3); }}}
+    int x;
+    for (int i = 1; i <= 5; ++i) {
+        for (int j = 1; j <= 5; ++j) {
+            std::cin >> x;
+            if (x == 1) {
+                std::cout << abs(i - 3) + abs(j - 3); }}}
 }
