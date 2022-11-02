b67f73ccb08ab9a7a27d754e95181eaacf3a3b4b
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:53:49 +0300

    Update 0318a.cpp

--- koroleva_e_l/0318a.cpp
+++ koroleva_e_l/0318a.cpp
@@ -1,17 +1,18 @@
-// https://codeforces.com/contest/318/problem/A
-#include<iostream>
+#include <iostream> // source:  https://codeforces.com/contest/318/problem/A
 #include <vector>
  
 int main()
 { 
-    int n,k,i,j;
-    std::cin>>n>>k;
-    std::vector<int>blah;
-    for (i=1;i<=n;i+=2) {
-        blah.push_back(i);}
-    for (j=2;j<=n;j+=2) {
-        blah.push_back(j);}
- 
-std::cout<<blah.at(k-1); 
-//громадные числа - жуть, опять проверку не прошло
+    int n = 0;
+    int k = 0;
+    std::cin >> n >> k;
+    std::vector<int> blah;
+    for (int i = 1; i <= n; i += 2) {
+        blah.push_back(i); // добавляем в вектор неч числа
+    }
+    for (int j = 2; j <= n; j += 2) {
+        blah.push_back(j); // добавляем в вектор чёт числа
+    }
+std::cout << blah.at(k - 1); 
+// громадные числа - жуть. На них ломается код
 }
