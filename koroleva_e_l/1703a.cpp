a5c435c6fcf088febcd279df692579aa5b4509b0
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:35:27 +0300

    Update 1703a.cpp

--- koroleva_e_l/1703a.cpp
+++ koroleva_e_l/1703a.cpp
@@ -1,18 +1,20 @@
-#include <iostream> //https://codeforces.com/contest/1703/problem/A
+#include <iostream> // source:  https://codeforces.com/contest/1703/problem/A
 #include <algorithm>
 #include <string>
 
-
 int main()
 {
-    int n=0;
-    std::cin>>n;
-    for (int i=0; i<n; i++) {
+    int n = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
         std::string str;
-        std::cin>>str;
-        std::transform(str.begin(), str.end(),str.begin(), ::toupper); //во какую приколюху нагуглила :D (преобразование строки в верхний регистр)
-        if (str=="YES"||str=="NO") {
-            std::cout<<"Yes\n";}
+        std::cin >> str;
+        std::transform(str.begin(), str.end(),str.begin(), ::toupper); // преобразование строки в верхний регистр (во какую приколюху нагуглила :D)
+        if (str == "YES" || str == "NO") {
+            std::cout << "Yes\n";
+        }
         else {
-            std::cout<<"No\n";}}
+            std::cout << "No\n";
+        }
+    }
 }
