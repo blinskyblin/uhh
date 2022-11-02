894517e847797e5a4446f84eef986c3b24befd32
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:04:15 +0300

    Update 0734a.cpp

--- koroleva_e_l/0734a.cpp
+++ koroleva_e_l/0734a.cpp
@@ -1,23 +1,30 @@
-#include <iostream> //https://codeforces.com/contest/734/problem/A
+#include <iostream> // source:  https://codeforces.com/contest/734/problem/A
 #include <string>
+
 int main()
 {
-    int n=0;
-    int a=0;
-    int d=0;
-    std::string s;
-    std::cin>>n>>s;
-    for(int i=0;i<n;i++) {
-        if(s[i]=='A') {
-            a+=1;}
+    int n = 0;
+    std::string s;    
+    int a = 0; // счёт Антона
+    int d = 0; // счёт Дани
+    std::cin >> n >> s;
+    for(int i = 0; i < n; ++i) {
+        if(s[i] == 'A') {
+            a += 1;
+        }
         else {
-            d+=1;}}
+            d += 1;
+        }
+    }
     
-    if(a>d) {
-        std::cout<<"Anton";}
+    if(a > d) {
+        std::cout << "Anton";
+    }
     else {
-        if(d>a) {
-        std::cout<<"Danik";}
+        if (d > a) {
+        std::cout << "Danik";
+    }
         else {
-        std::cout<<"Friendship";}}
-}
+        std::cout << "Friendship";
+        }
+    }
