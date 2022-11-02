46326ff963801f153dbe6e8c44506846b83256ad
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:31:05 +0300

    Update 0263a.cpp

--- koroleva_e_l/0263a.cpp
+++ koroleva_e_l/0263a.cpp
@@ -1,12 +1,14 @@
-// https://codeforces.com/contest/263/problem/A
-#include <iostream>
+#include <iostream> //source:  https://codeforces.com/contest/263/problem/A
  
 int main()
 {
-    int x;
+    int x = 0;
     for (int i = 1; i <= 5; ++i) {
         for (int j = 1; j <= 5; ++j) {
             std::cin >> x;
             if (x == 1) {
-                std::cout << abs(i - 3) + abs(j - 3); }}}
+                std::cout << abs(i - 3) + abs(j - 3); 
+            }
+        }
+    }
 }
