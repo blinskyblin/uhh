ba0d6402b9b17b09193ff9ba6ddd8ac3985edae9
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:37:42 +0300

    Update 0271a.cpp

--- koroleva_e_l/0271a.cpp
+++ koroleva_e_l/0271a.cpp
@@ -1,18 +1,21 @@
-// https://codeforces.com/contest/271/problem/A
-#include <iostream>
- 
+#include <iostream> // source:  https://codeforces.com/contest/271/problem/A
+
 int main()
 {
-    int y=0;
-    std::cin>> y;
- 
-    for(int i=y+1; i<=10000; i++){  
-        
-        int a=i/1000;
-        int b=(i-1000*a)/100;
-        int c=(i-1000*a-100*b)/10;
-        int d=i%10;
-        if (a!=b && a!=c && a!=d && b!=c && b!=d && c!=d) {
-        std::cout<<i;
-        break;} }
+    int y = 0;
+    int a = 0; // первая цифра искомого года
+    int b = 0; // вторая цифра
+    int c = 0; // третья
+    int d = 0; // четвёртая
+    std::cin >> y;
+    for (int i = y+1; i <= 10000; ++i) {  
+        a = i / 1000;
+        b = (i-1000*a)/100;
+        c = (i - 1000 * a - 100 * b) / 10;
+        d = i % 10;
+        if (a != b && a != c && a != d && b != c && b != d && c != d) { //условие неравенства цифр года
+        std::cout << i;
+        break;
+        } 
+    }
 }
