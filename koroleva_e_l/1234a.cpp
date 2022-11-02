7a551b313f7de9469b61ed070f5cd73d08a358a2
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:11:35 +0300

    Update 1234a.cpp

--- koroleva_e_l/1234a.cpp
+++ koroleva_e_l/1234a.cpp
@@ -1,19 +1,23 @@
-#include<iostream> //https://codeforces.com/problemset/problem/1234/A
+#include<iostream> // source:  https://codeforces.com/problemset/problem/1234/A
 
 int main()
 {
-    int q=0;
-    int x;
-    std::cin>>q;
-    for (int i=0; i<q; i++) {
-        int n=0;
-        int sum=0;
-        std::cin>>n;
-        for(int j=0;j<n;j++){
-            std::cin>>x;
-            sum+=x;}
-        if (sum%n==0) {
-            std::cout<<sum/n<<"\n";}
+    int q = 0;
+    int n = 0;    
+    int ai = 0;
+    std::cin >> q;
+    for (int i = 0; i < q; ++i) {
+        int sum = 0;
+        std::cin >> n;
+        for (int j = 0; j < n; ++j) {
+            std::cin >> ai;
+            sum += ai;
+        }
+        if (sum % n == 0) {
+            std::cout << sum / n << "\n";
+        }
         else {
-            std::cout<<sum/n+1<<"\n";}}
+            std::cout << sum / n + 1 << "\n";
+        }
+    }
 }
