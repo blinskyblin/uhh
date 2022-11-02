15479d2ddd1e219e877e5dc029d8fa15d7c3fdb9
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:27:00 +0300

    Update 1678a.cpp

--- koroleva_e_l/1678a.cpp
+++ koroleva_e_l/1678a.cpp
@@ -1,34 +1,42 @@
-#include <iostream> //https://codeforces.com/problemset/problem/1678/A
+#include <iostream> // source:  https://codeforces.com/problemset/problem/1678/A
 #include <vector>
 #include <algorithm>
 
 int main()
 {
-    int t=0;
-    int n=0;
-    int x;
-    std::cin>>t;
-    for (int i=0; i<t; i++) {
-        int y=0; //кол-во нулей в векторе
-        int z=0; //кол-во повторение в векторе
-        std::vector<int>V; 
-        std::cin>>n;
-        for (int j=0; j<n; j++) {
-            std::cin>>x;
-            V.push_back(x);}
+    int t = 0;
+    int n = 0;
+    int a = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; ++i) {
+        int y = 0; // кол-во нулей в векторе
+        int z = 0; // кол-во повторений в векторе
+        std::vector<int> V; 
+        std::cin >> n;
+        for (int j = 0; j < n; ++j) {
+            std::cin >> a;
+            V.push_back(a);
+        }
         std::sort (V.begin(), V.end());
-    if (V[0]==0) {
-            y+=1;} //отдельно запишем, чтоб потом не выходить за пределы вектора
-    for (int k=1; k<n; k++) {
-        if (V[k]==0) {
-            y+=1;}
-        if (V[k-1]==V[k]) {
-            z+=1;}} 
-            
-    if (y!=0) {
-        std::cout<<n-y<<std::endl;}
-    if (y==0&&z!=0) { 
-        std::cout<<n<<std::endl;}
-    if (y==0&&z==0){ 
-        std::cout<<n+1<<std::endl;}}
+        if (V[0] == 0) {
+            y += 1;
+        } // отдельно запишем, чтоб потом не выходить за пределы вектора
+        for (int k = 1; k < n; ++k) {
+            if (V[k] == 0) {
+                y += 1;
+            }
+            if (V[k - 1] == V[k]) {
+                z += 1;
+            }
+        }         
+        if (y != 0) {
+            std::cout << n - y << std::endl;
+        }
+        if (y ==0 && z != 0) { 
+            std::cout << n << std::endl;
+        }
+        if (y == 0 && z == 0 ) { 
+            std::cout << n + 1 << std::endl;
+        }
+    }
 }
