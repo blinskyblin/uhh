8429c3595f8b0cdc2e1ad8e9a51b7911960be39f
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:49:04 +0300

    Update 0313a.cpp

--- koroleva_e_l/0313a.cpp
+++ koroleva_e_l/0313a.cpp
@@ -1,15 +1,19 @@
-#include <iostream> //https://codeforces.com/contest/313/problem/A
+#include <iostream> // source:  https://codeforces.com/contest/313/problem/A
 
 int main()
 {
-	int n;
-	std::cin>>n;
-	if(n>=0) {
-		std::cout<<n;}
+	int n = 0;
+	std::cin >> n;
+	if (n >= 0) {
+		std::cout << n;
+	}
 	else {
-		n=-n;
-		if ((n/100)*10+n%10<n/10) {
-			std::cout<<-((n/100)*10+n%10);}
+		n =- n; // для упрощения понимания (неразберихи со знаками)
+		if ((n / 100) * 10 + n % 10 < n / 10) {
+			std::cout << - ((n / 100) * 10 + n % 10);
+		}
 		else {
-			std::cout<<-n/10;}}
+			std::cout << - n / 10;
+		}
+	}
 }
