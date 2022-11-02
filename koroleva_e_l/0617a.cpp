f8b6897f8b32c4855f55b85471b5231775dff76f
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:55:11 +0300

    Update 0617a.cpp

--- koroleva_e_l/0617a.cpp
+++ koroleva_e_l/0617a.cpp
@@ -1,14 +1,14 @@
-// https://codeforces.com/contest/617/problem/A
-#include <iostream>
+#include <iostream> // source:  https://codeforces.com/contest/617/problem/A
  
 int main()
 {
-    int x=0;
-    std::cin>> x;
-    int i=(x/5);
-    if (x%5!=0){
-    std::cout<<i+1; }
+    int x = 0;
+    std::cin >> x;
+    int i = (x / 5);
+    if (x % 5 != 0) {
+        std::cout << i + 1; 
+    }
     else {
-        std::cout<<i;
+        std::cout << i;
     }
 }
