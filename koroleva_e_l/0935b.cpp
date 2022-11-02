69dfef5934930e0e20d3036850b4ccbcaae86010
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:08:08 +0300

    Update 0935b.cpp

--- koroleva_e_l/0935b.cpp
+++ koroleva_e_l/0935b.cpp
@@ -1,21 +1,24 @@
-#include <iostream> //https://codeforces.com/problemset/problem/935/B
+#include <iostream> // source:  https://codeforces.com/problemset/problem/935/B
 #include <string>
 
 int main() {	
-    int n=0;
-	int x=0;
-	int y=0;
-	int d=0; //деньги
-	int i=0;
-	std::string S;
-	std::cin>>n;
-	std::cin>>S;
-	for (i=0; i<n; i++) {
-		if (S[i]=='R') {
-			x+=1;}
-		if (S[i]=='U') {
-			y+=1;}
-		if (x==y && S[i]==S[i+1]) {//это если не развернулся, а прошёл через ворота
-			d+=1;}}
+  int n = 0;	
+  std::string S;
+	int x = 0; // координата х
+	int y = 0; // координата у
+	int d = 0; // деньги
+	std::cin >> n;
+	std::cin >> S;
+	for (int i = 0; i < n; ++i) {
+		if (S[i] == 'R') {
+			x += 1;
+		}
+		if (S[i] == 'U') {
+			y += 1;
+		}
+		if (x == y && S[i] == S[i + 1]) {//это если не развернулся, а прошёл через ворота
+			d += 1;
+		}
+	}
 	std::cout<<d;
 }
