5a49fb2a69fc5ff2ff7f61464d9aa5a470ced6ee
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:29:32 +0300

    Update 0114a.cpp

--- koroleva_e_l/0114a.cpp
+++ koroleva_e_l/0114a.cpp
@@ -1,17 +1,17 @@
-#include <iostream>  // https://codeforces.com/problemset/problem/114/A
+#include <iostream>  //source:  https://codeforces.com/problemset/problem/114/A
 #include <cmath>
 
 int main()
 {
-    long long k=0;
-    long long l=0;
-    int a=0;
-    int b=0;
-    std::cin>>k>>l;
-    a=(std::log(l)/std::log(k)); //в какую округлённую степень возвести k, чтоб получить l
-    b=pow(k,a); //k в округлённой степени
-    if(b==l) {
-        std::cout << "YES" << '\n' << a-1;     
+    long long k = 0;
+    long long l = 0;
+    int a = 0;
+    int b = 0;
+    std::cin >> k >> l;
+    a = (std::log(l) / std::log(k)); //в какую округлённую степень возвести k, чтоб получить l
+    b = pow(k, a); //k в округлённой степени
+    if (b == l) {
+        std::cout << "YES" << '\n' << a - 1;     
     }
     else {
         std::cout << "NO";
