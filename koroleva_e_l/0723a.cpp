c60b673d5b5854b197a8dbcaf45317a738d12995
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:59:34 +0300

    Update 0723a.cpp

--- koroleva_e_l/0723a.cpp
+++ koroleva_e_l/0723a.cpp
@@ -1,9 +1,12 @@
-#include <iostream> //https://codeforces.com/problemset/problem/723/A
+#include <iostream> // source:  https://codeforces.com/problemset/problem/723/A
 #include <algorithm>
  
 int main()
 {
-    int x1,x2,x3;
-    std::cin>>x1>>x2>>x3;
-    std::cout<<std::max(x1,std::max(x2,x3))-std::min(x1,std::min(x2,x3));
+    int x1 = 0;
+    int x2 = 0;
+    int x3 = 0;
+    std::cin >> x1 >> x2 >> x3;
+    std::cout << std::max(x1, std::max(x2, x3)) - std::min(x1, std::min(x2, x3));
 }
+
