449f1382cdf7dc354ee27e5e7d0a55d8496ff70e
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:01:26 +0300

    Update 0732a.cpp

--- koroleva_e_l/0732a.cpp
+++ koroleva_e_l/0732a.cpp
@@ -1,13 +1,16 @@
-#include <iostream> //https://codeforces.com/problemset/problem/732/A
+#include <iostream> // source:  https://codeforces.com/problemset/problem/732/A
 
 int main()
 {
-    int k,r,sum;
-    int c=1; //начальное кол-во лопат
-    std::cin>>k>>r;
-    sum=k;
-    while((sum%10!=0)&&(sum%10!=r)) { //непонятно как с for делать, так что вот
-        c+=1;
-        sum+=k;}
-    std::cout<<c;
+    int k = 0;
+    int r = 0;
+    int sum = 0;
+    int c = 1; //начальное кол-во лопат
+    std::cin >> k >> r;
+    sum = k;
+    while ((sum % 10 != 0) && (sum % 10 != r)) { //непонятно как с for делать, так что уот
+        c += 1;
+        sum += k;
+    }
+    std::cout << c;
 }
