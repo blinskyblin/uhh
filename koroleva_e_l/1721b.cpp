24107511d9de01fa9e697060a460c13996213941
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 10:38:51 +0300

    Update 1721b.cpp

--- koroleva_e_l/1721b.cpp
+++ koroleva_e_l/1721b.cpp
@@ -1,20 +1,23 @@
-#include <iostream> //https://codeforces.com/problemset/problem/1721/B
+#include <iostream> // source:  https://codeforces.com/problemset/problem/1721/B
 #include <algorithm>
 
 int main()
 {
-    int t=0;
-    int n=0;
-    int m=0;
-    int sx=0;
-    int sy=0;
-    int d=0;
-    std::cin>>t;
-    for (int i=0; i<t; i++) {
-	std::cin>>n >> m>> sx>> sy >> d;
-	if (std::min(sx - 1, m - sy) <= d && std::min(n - sx, sy - 1) <= d) {
-	    std::cout<<-1<<"\n";}
-	else {
-	    std::cout<<n + m - 2<<'\n';}}
-
+    int t = 0;
+    int n = 0;
+    int m = 0;
+    int sx = 0;
+    int sy = 0;
+    int d = 0;
+    std::cin >> t;
+    for (int i = 0; i < t; ++i) {
+	    std::cin >> n >> m >> sx >> sy >> d;
+	    if (std::min(sx - 1, m - sy) <= d && std::min(n - sx, sy - 1) <= d) {
+	        std::cout << -1 << "\n";
+	    }
+	    else {
+	        std::cout << n + m - 2 << '\n';
+	    }
+    }
 }
+
