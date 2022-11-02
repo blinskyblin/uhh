778e7f816bc48cfd0b75290adddd9a0d6459b8ad
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:27:36 +0300

    Update 0004a.cpp

--- koroleva_e_l/0004a.cpp
+++ koroleva_e_l/0004a.cpp
@@ -1,19 +1,21 @@
-// https://codeforces.com/contest/4/problem/A
-#include <iostream>
- 
+#include <iostream> //source:  https://codeforces.com/contest/4/problem/A
+
 int main()
 {
-    int w=0;
-    std::cin>> w;
-    if (w%2==0 && w>=4) {
-        int x=0;
-        for (int i=2; i+2<=w; i+=2) {
-            if ((w-i)%2==0) {
-                x+=1; }}
-        if (x!=0) {
-        std::cout << "YES"; }}
-    
+    int w = 0;
+    std::cin >> w;
+    if (w % 2 == 0 && w >= 4) {
+        int x = 0;
+        for (int i = 2; i + 2 <= w; i += 2) {
+            if ((w-i) % 2 == 0) {
+                x += 1; 
+            }
+        }
+        if (x != 0) {
+            std::cout << "YES"; 
+        }
+    }
     else {
-    std::cout << "NO"; 
+        std::cout << "NO"; 
     }
 }
