7aeb448eb4e77db0580c56cc3d62aeb4531c757f
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-02 09:57:55 +0300

    Update 0630a.cpp

--- koroleva_e_l/0630a.cpp
+++ koroleva_e_l/0630a.cpp
@@ -1,7 +1,6 @@
-// https://codeforces.com/contest/630/problem/A
-#include <iostream>
+#include <iostream> // source:  https://codeforces.com/contest/630/problem/A
  
 int main()
 {
-    std::cout<<"25"; //ой ору
+    std::cout << "25"; // ой ору
 }
