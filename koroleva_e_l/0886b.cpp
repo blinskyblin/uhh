84028d72f5164a2418a468b41f7b6fd879c8bfd8
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-11-10 13:16:03 +0300

    Update and rename 886b.cpp to 0886b.cpp

--- koroleva_e_l/0886b.cpp
+++ koroleva_e_l/0886b.cpp
@@ -6,17 +6,19 @@ int main()
 {
     int n = 0;
     int ai = 0;
+    int count = 0;
     std::vector<int> v;
     std::cin >> n;
     for (int i = 0; i < n; ++i) {
         std::cin >> ai;
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < count; ++j) {
             if (v[j] == ai) {
                 v.erase(std::find(v.begin(), v.end(), ai));
                 break;
             }
         }
         v.push_back(ai);
+        count+=1;
     }
     std::cout << v[0];
 }
