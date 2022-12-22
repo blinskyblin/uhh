691652c32291ef2012c952902ed68b15d395ea6d
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-12-22 18:25:15 +0300

    Update bpm_22_2_3.cpp

--- koroleva_e_l/bpm_22_2_3.cpp
+++ koroleva_e_l/bpm_22_2_3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 int main()
 {
     int i = 0;
@@ -8,42 +8,44 @@ int main()
     int dlina = 0;
     int ser = 0; //середина
     std::string str;
-    std::string result;
-    std::cin>>str;
-    dlina = str.length(); 
+    std::cin >> str;
+    dlina = str.length();
+    std::vector<char> result;
     
-    if ((dlina == 1) || (dlina == 2)) { //два первые скучные нудные случаи чур вручную
+    
+    if ((dlina == 1) || (dlina == 2)) { //два первые скучные случаи чур вручную
         if (dlina == 1) { 
-            result+str[0];
+            result.push_back(str[0]);
         }
     
         if (dlina == 2) {
-            result+str[0];
-            result+str[1];
+            result.push_back(str[0]);
+            result.push_back(str[1]);
         } 
     }
     
     else {
-        if (dlina%2==0) { //если длина str чёт
-            ser = dlina/2;
-            result+str[ser];
-            for (i=1; i<ser-1; i++) {
-                result+str[ser+i];
-                result+str[ser-i];
+        if (dlina % 2 == 0) { //если длина str чёт
+            ser = dlina / 2 - 1; //именно индекс
+            result.push_back(str[ser]);
+            for (i = 1; i <= ser; i++) {
+                result.push_back(str[ser + i]);
+                result.push_back(str[ser - i]);
             }
-        result+str[2*ser];
+        result.push_back(str[dlina-1]);
         }
     
-        if (dlina%2==1) { //если длина str нечёт
-            ser=dlina/2+1;
-            result+str[ser];
-            for (i=1; i<ser-1; i++) {
-                result+str[ser-i];
-                result+str[ser+i];
+        if (dlina % 2 == 1) { //если длина str нечёт
+            ser = dlina / 2 ;
+            result.push_back(str[ser]);
+            for (i = 1; i <= ser; i++) {
+                result.push_back(str[ser - i]);
+                result.push_back(str[ser + i]);
             }
-        result+str[2*ser];
         }
     }
     
-    std::cout<<result;
+    for (k = 0; k < dlina; k++) {
+        std::cout << result[k];
+    }
 }
