4f40ef016be7ae365113144e0fdba269e991c4df
blinskyblin <114044347+blinskyblin@users.noreply.github.com>
2022-12-22 14:02:09 +0300

    Create bpm_22_2_3.cpp

---
+++ koroleva_e_l/bpm_22_2_3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int i = 0;
+    int k = 0;
+    int dlina = 0;
+    int ser = 0; //середина
+    std::string str;
+    std::string result;
+    std::cin>>str;
+    dlina = str.length(); 
+    
+    if ((dlina == 1) || (dlina == 2)) { //два первые скучные нудные случаи чур вручную
+        if (dlina == 1) { 
+            result.insert(result.length(), str[0]);
+        }
+    
+        if (dlina == 2) {
+            result.insert(result.length(),str[0]);
+            result.insert(result.length(),str[1]);
+        } 
+    }
+    
+    else {
+        if (dlina%2==0) { //если длина str чёт
+            ser = dlina/2;
+            result.insert(result.length(),str[ser]);
+            for (i=1; i<ser-1; i++) {
+                result.insert(result.length(),str[ser+i]);
+                result.insert(result.length(),str[ser-i]);
+            }
+        result.insert(result.length(),str[2*ser]);
+        }
+    
+        if (dlina%2==1) { //если длина str нечёт
+            ser=dlina/2+1;
+            result.insert(result.length(),str[ser]);
+            for (i=1; i<ser-1; i++) {
+                result.insert(result.length(),str[ser-i]);
+                result.insert(result.length(),str[ser+i]);
+            }
+        result.insert(result.length(),str[2*ser]);
+        }
+    }
+    
+    std::cout<<result;
+}
